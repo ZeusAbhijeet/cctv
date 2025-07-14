32b3102f2f42440ee47c643d0e842da12999bd7e
Abhijeet Singh <62468954+ZeusAbhijeet@users.noreply.github.com>
2025-07-14 21:21:26 +0530

    Update README.md and Configs.jsx

--- README.md
+++ README.md
@@ -6,11 +6,14 @@
 - Expo
 - NPM
 - An Android Device with USB Debugging on or an Android Emulator
+- The backend server (TODO: Link repo)
 
 ## Installation
 
 - Extract the folder
 - Replace Google Maps API key in [App.js](App.js) and [app.json](app.json)
+- Replace the IP Address of the backend server in [cctvMaps.jsx](src/screens/cctvMaps.jsx), [profile.jsx](src/screens/profile.jsx) and [Tickets.jsx](src/screens/Tickets.jsx).
+- Add your firebase config to [Configs.jsx](src/configs/Configs.jsx)
 - Open terminal
 ```bash
 cd CCTV
--- src/configs/Configs.jsx
+++ src/configs/Configs.jsx
@@ -9,13 +9,13 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAU8-ElBrKEXYZXBVVjckIT3W3HMZjSDvo",
-  authDomain: "cctv-locator-police-hackathon.firebaseapp.com",
-  projectId: "cctv-locator-police-hackathon",
-  storageBucket: "cctv-locator-police-hackathon.appspot.com",
-  messagingSenderId: "121112930920",
-  appId: "1:121112930920:web:5213b67de3603816cb29dd",
-  measurementId: "G-SYFFGWPFY8"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
